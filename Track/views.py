34eca178b25488f886faaced76bc84ffbd110ddc
Ankitasharma <sharmanki2007@gmail.com>
2026-01-27 01:06:30 +0530

    fix

--- Track/views.py
+++ Track/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from . serializers import EchoSerializer
 
 @api_view(["POST"])
-def echo_view(request):
+def register_echo(request):
     serializer = EchoSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
