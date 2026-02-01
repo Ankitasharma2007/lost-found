7f96c10786b63c2d1911934c878389fb6205e3a8
Ankitasharma <sharmanki2007@gmail.com>
2026-02-01 15:17:26 +0530

    fix

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from .views import RegisterView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('login/' , TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+
+    ]
