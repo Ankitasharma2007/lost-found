81918afc3c0f61a4017ff018f99b63deec16e5d3
Ankitasharma <sharmanki2007@gmail.com>
2026-02-02 19:49:38 +0530

    fix

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -49,3 +49,34 @@ class FoundListView(APIView):
         query = Found.objects.filter(lost_item__isnull = True)
         serializer = FoundListSerializer(query,many=True)
         return Response(serializer.data)
+    
+class MatchLostFoundView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def post(self,request):
+        lost_id = request.data.get("lost_id")
+        found_id = request.data.get("found_id")
+
+        if not lost_id or not found_id : 
+            return Response(
+                {
+                    "error": "Pls send a valid requst"
+                },
+                status= status.HTTP_400_BAD_REQUEST
+            )
+
+        lost_obj = get_object_or_404(Lost,lost_id)
+        found_obj = get_object_or_404(Found,found_id)
+
+        lost_obj.resolved = True
+        lost_obj.found_item = found_obj
+        lost_obj.save()
+
+        return Response(
+            {
+                "object matched successful"
+            },status=status.HTTP_200_OK
+        )
+
+        
+
